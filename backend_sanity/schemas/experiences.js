2d3dc32cf5be49098e257fc5f3bfe342a510a219
Claudette Cabahug <claudette.cabahug@outlook.com>
2022-03-20 12:58:08 +1300

    add codes for work experience page

--- backend_sanity/schemas/experiences.js
+++ backend_sanity/schemas/experiences.js
@@ -12,7 +12,7 @@ export default {
       name: 'works',
       title: 'Works',
       type: 'array',
-      of:[{type: 'workExperience'}]
+      of:[{ type: 'workExperience' }]
     }
   ]
 }
--- backend_sanity/schemas/schema.js
+++ backend_sanity/schemas/schema.js
@@ -3,10 +3,11 @@ import createSchema from 'part:@sanity/base/schema-creator'
 
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
-import testimonials from './testimonials'
 import about from './about'
 import contact from './contact'
 import experiences from './experiences'
+// import skills from './skills'
+import workExperience from './workExperience'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -15,9 +16,10 @@ export default createSchema({
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    testimonials,
     about,
     contact,
-    experiences
+    experiences,
+    // skills,
+    workExperience
   ]),
 })
