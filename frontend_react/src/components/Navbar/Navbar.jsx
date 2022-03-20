f7affb09e8208170d903339dac14ad0e72ca29f0
Claudette Cabahug <claudette.cabahug@outlook.com>
2022-03-20 22:48:57 +1300

    hide the hamburger menu when screen reaches 900px

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -35,11 +35,13 @@ const Navbar = () => {
               transition={{ duration: 0.85, ease: 'easeOut' }}
             >
               <HiX onClick={() => setToggle(false)} />
-              {['home', 'about', 'contact', 'work', 'skills'].map((item) => (
-                <li key={item}>
-                  <a href={`#${item}`} onClick={() => setToggle(false)}>{item}</a>
-                </li>
-              ))}
+              <ul>
+                {['home', 'about', 'contact', 'work', 'skills'].map((item) => (
+                  <li key={item}>
+                    <a href={`#${item}`} onClick={() => setToggle(false)}>{item}</a>
+                  </li>
+                ))}
+              </ul>
             </motion.div>
           )}
 
