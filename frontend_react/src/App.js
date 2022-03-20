88241ea1cce5746da15d620e1480944d300b783f
Claudette Cabahug <claudette.cabahug@outlook.com>
2022-03-20 14:54:38 +1300

    create the Navbar component and add it to App.js

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
 import { About, Footer, Header, Skills, Work } from './container'
+import { Navbar } from './components'
 
 const App = () => {
   return (
     <div className='app'>
+      <Navbar />
       <Header />
       <About />
       <Work />
