92f7c075f0c582319b89cda26eece2e95946c392
Claudette Cabahug <claudette.cabahug@outlook.com>
2022-03-20 13:10:43 +1300

    add schema for works page

--- backend_sanity/schemas/schema.js
+++ backend_sanity/schemas/schema.js
@@ -8,6 +8,7 @@ import contact from './contact'
 import experiences from './experiences'
 import skills from './skills'
 import workExperience from './workExperience'
+import works from './works'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -20,6 +21,7 @@ export default createSchema({
     contact,
     experiences,
     skills,
-    workExperience
+    workExperience,
+    works
   ]),
 })
