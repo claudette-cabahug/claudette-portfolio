7847e3f53750b5dd67464e2dfb4d0cb302780d2d
Claudette Cabahug <claudette.cabahug@outlook.com>
2022-03-24 00:35:27 +1300

    fix bug that caused data from sanity to not show on client

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -6,20 +6,15 @@ import { images } from '../../constants'
 import './About.scss'
 import { urlFor, client } from '../../client'
 
-// const abouts = [
-//   { title: 'Fullstack Web Development', description: 'I am a good web developer.', imgUrl: images.about01 },
-//   { title: 'Frontend Development', description: 'I am a good web developer.', imgUrl: images.about02 },
-//   { title: 'Backend Development', description: 'I am a good web developer.', imgUrl: images.about03 },
-// ]
-
 const About = () => {
   const [abouts, setAbouts] = useState([])
 
   useEffect(() => {
     const query = '*[_type == "abouts"]'
 
-    client.fetch(query)
-      .then((data) => setAbouts(data))
+    client.fetch(query).then((data) => {
+      setAbouts(data)
+    })
   }, [])
   
 
@@ -36,7 +31,7 @@ const About = () => {
             className='app__profile-item'
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className='bold-text' style={{ marginTop: 20 }}>{about.title}</h2>
             <p className='p-text' style={{ marginTop: 10 }}>{about.description}</p>
           </motion.div>
